3422f8cdae5e10dfa345f144167ee157604c9165
Palash Chandra Das <85504305+PalashChandraDas@users.noreply.github.com>
2022-06-27 21:10:59 +0600

    Update single_inheritance.java

--- Inheritance/single_inheritance.java
+++ Inheritance/single_inheritance.java
@@ -7,7 +7,7 @@ class Engineer extends Employee{  // Derived/Sub/Child class
     
     public static void main(String[] args){
       
-        Engineer engineer = new Engineer(); //engineer object
+        Engineer engineer = new Engineer(); //engineer is object
       
         System.out.printf("Engineer's salary: %d Tk.\n", engineer.salary);
         System.out.printf("Engineer's bonus: %d Tk.\n", engineer.bonus);
